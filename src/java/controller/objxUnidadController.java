d5b4c117bdd6c953b8fcf7e40847836c60b4c541
migueljazz <miguel.rcr@gmail.com>
2014-09-26 13:00:08 -0500

    2609-4

--- src/java/controller/objxUnidadController.java
+++ src/java/controller/objxUnidadController.java
@@ -5,6 +5,7 @@
  */
 //ESTE ES PARA EL REPORTE
 package controller;
+
 import dao.DocumentoDAO;
 import daoimpl.DocumentoDaoImpl;
 import maping.*;
@@ -12,9 +13,7 @@ import java.io.Serializable;
 import javax.faces.application.FacesMessage;
 import javax.servlet.ServletContext;
 
-
 //
-
 import java.util.HashMap;
 
 import javax.faces.bean.ManagedBean;
@@ -24,177 +23,167 @@ import javax.servlet.http.HttpSession;
 import maping.Usuario;
 //
 
-
 @ManagedBean
 @ViewScoped
-public class objxUnidadController implements Serializable{
-    
-    
-private static final long serialVersionUID = 8797816477254175229L;
-FacesContext context;
-ServletContext serveltcontext;
-private int anioActual;
-private int opcionFormato;
-private int mesInicio;
-private int mesFin ;
-private int mesActual ;
-private DocumentoDAO dd;
+public class objxUnidadController implements Serializable {
 
+    private static final long serialVersionUID = 8797816477254175229L;
+    FacesContext context;
+    ServletContext serveltcontext;
+    private int anioActual;
+    private int opcionFormato;
+    private int mesInicio;
+    private int mesFin;
+    private int mesActual;
+    private DocumentoDAO dd;
 
-private String USUARIO;
+    private String USUARIO;
 
     public objxUnidadController() {
-        dd= new DocumentoDaoImpl();
+        dd = new DocumentoDaoImpl();
     }
 
-public void mostrarReporRegModPres(){
+    public void mostrarReporRegModPres() {
 
-context = FacesContext.getCurrentInstance (); 
-serveltcontext = (ServletContext) context.getExternalContext().getContext();
-ReporteController repor;
-HashMap<String, Object> parametros = new HashMap<String, Object>();
-parametros.clear();
+        context = FacesContext.getCurrentInstance();
+        serveltcontext = (ServletContext) context.getExternalContext().getContext();
+        ReporteController repor;
+        HashMap<String, Object> parametros = new HashMap<String, Object>();
+        parametros.clear();
 //parametros.put("anio", 2014/*getAnioActual()*/);
 /*
-Calendar fecha = new GregorianCalendar();
-int mes = fecha.get(Calendar.MONTH);
+         Calendar fecha = new GregorianCalendar();
+         int mes = fecha.get(Calendar.MONTH);
 
-int año = fecha.get(Calendar.YEAR);
-mes++;
-int dia = fecha.get(Calendar.DAY_OF_MONTH);
-int hora = fecha.get(Calendar.HOUR_OF_DAY);
-int minuto = fecha.get(Calendar.MINUTE);
-int segundo = fecha.get(Calendar.SECOND);
-System.out.println("Fecha Actual: "
-                   + dia + "/" + (mes+1) + "/" + año);
-System.out.printf("Hora Actual: %02d:%02d:%02d %n",
-                                      hora, minuto, segundo);
-*/
+         int año = fecha.get(Calendar.YEAR);
+         mes++;
+         int dia = fecha.get(Calendar.DAY_OF_MONTH);
+         int hora = fecha.get(Calendar.HOUR_OF_DAY);
+         int minuto = fecha.get(Calendar.MINUTE);
+         int segundo = fecha.get(Calendar.SECOND);
+         System.out.println("Fecha Actual: "
+         + dia + "/" + (mes+1) + "/" + año);
+         System.out.printf("Hora Actual: %02d:%02d:%02d %n",
+         hora, minuto, segundo);
+         */
 
-FacesContext context = FacesContext.getCurrentInstance (); 
-System.out.println("context"+context);
-ServletContext sc = (ServletContext) context.getExternalContext ().getContext();
+        FacesContext context = FacesContext.getCurrentInstance();
+        System.out.println("context" + context);
+        ServletContext sc = (ServletContext) context.getExternalContext().getContext();
 //System.out.println("sc = "+sc.getRealPath ("escudo.jpg"));
 //String imagen=sc.getRealPath ("/pages/images/escudo.jpg");
 //String subreport=sc.getRealPath ("/reportes/planes_presupuesto/");
-System.out.println("sc = "+sc.getRealPath ("/reportes/"));
+        System.out.println("sc = " + sc.getRealPath("/reportes/"));
 //parametros.put("SUBREPORT_DIR", subreport);
 //parametros.put("logo", imagen);
 //parametros.put("subrep3", true);
 
-/*if( fuenteElegido.equals("99") ){
-	 repor= ReporteController.getInstance("reporteCN_TF");
-}else{*/
+        /*if( fuenteElegido.equals("99") ){
+         repor= ReporteController.getInstance("reporteCN_TF");
+         }else{*/
 //parametros.put("fuente", getFuenteElegido() );
-repor= ReporteController.getInstance("prueba3");
+        repor = ReporteController.getInstance("prueba3");
 //}
-categoriaServicio categoriaServicio= new categoriaServicio();
-repor.setConexion(categoriaServicio.getConexion() );
-repor.setTipoFormato(opcionFormato);   /// para tIPO FORMATO  08/05
-FacesMessage message=null;
-boolean rpt=false;	
+        categoriaServicio categoriaServicio = new categoriaServicio();
+        repor.setConexion(categoriaServicio.getConexion());
+        repor.setTipoFormato(opcionFormato);   /// para tIPO FORMATO  08/05
+        FacesMessage message = null;
+        boolean rpt = false;
 
+        /*parametros.put("udid", udIdElegido );
+         parametros.put("udcod", udCodElegido );
+         parametros.put("uddsc", dependenciaService.obtenerDepxUdcod(udCodElegido).getDescripcion() );
+         parametros.put("usuario", obtenerUsuario() );
 	
-	
-	/*parametros.put("udid", udIdElegido );
-	parametros.put("udcod", udCodElegido );
-	parametros.put("uddsc", dependenciaService.obtenerDepxUdcod(udCodElegido).getDescripcion() );
-	parametros.put("usuario", obtenerUsuario() );
-	
-	parametros.put("SUBREPORT_DIR", obtenerReporteDir() );	*/
+         parametros.put("SUBREPORT_DIR", obtenerReporteDir() );	*/
         parametros.put("USUARIO", getUSUARIO());
-       // parametros.put("oficina","oficina oli");
+        // parametros.put("oficina","oficina oli");
         parametros.put("logo", getLogo());
-        parametros.put("oficina",getOficina());
-       // parametros.put("USUARIO","miguel" ); 
-	repor.addMapParam(parametros);
-	rpt=repor.ejecutaReporte(context,serveltcontext);	
-		
+        parametros.put("oficina", getOficina());
+        // parametros.put("USUARIO","miguel" ); 
+        repor.addMapParam(parametros);
+        rpt = repor.ejecutaReporte(context, serveltcontext);
 
-        
-if(!rpt && message==null){
-	//no tiene hojas	
-	message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje","No hay datos para generar reporte");
-	FacesContext.getCurrentInstance().addMessage(null, message);
-}
-}
+        if (!rpt && message == null) {
+            //no tiene hojas	
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje", "No hay datos para generar reporte");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
 
+    public void mostrarReporteNotasDeriv() {
 
-
-public void mostrarReporteNotasDeriv(){
-
-context = FacesContext.getCurrentInstance (); 
-serveltcontext = (ServletContext) context.getExternalContext().getContext();
-ReporteController repor;
-HashMap<String, Object> parametros = new HashMap<String, Object>();
-parametros.clear();
-FacesContext context = FacesContext.getCurrentInstance (); 
-System.out.println("context"+context);
-ServletContext sc = (ServletContext) context.getExternalContext ().getContext();
-System.out.println("sc = "+sc.getRealPath ("/reportes/"));
-repor= ReporteController.getInstance("NotasDerivadas");
-categoriaServicio categoriaServicio= new categoriaServicio();
-repor.setConexion(categoriaServicio.getConexion() );
-repor.setTipoFormato(opcionFormato);   /// para tIPO FORMATO  08/05
-FacesMessage message=null;
-boolean rpt=false;	
+        context = FacesContext.getCurrentInstance();
+        serveltcontext = (ServletContext) context.getExternalContext().getContext();
+        ReporteController repor;
+        HashMap<String, Object> parametros = new HashMap<String, Object>();
+        parametros.clear();
+        FacesContext context = FacesContext.getCurrentInstance();
+        System.out.println("context" + context);
+        ServletContext sc = (ServletContext) context.getExternalContext().getContext();
+        System.out.println("sc = " + sc.getRealPath("/reportes/"));
+        repor = ReporteController.getInstance("NotasDerivadas");
+        categoriaServicio categoriaServicio = new categoriaServicio();
+        repor.setConexion(categoriaServicio.getConexion());
+        repor.setTipoFormato(opcionFormato);   /// para tIPO FORMATO  08/05
+        FacesMessage message = null;
+        boolean rpt = false;
         parametros.put("usuario", getUSUARIO());
         //parametros.put("oficina","oficina oli");
         parametros.put("logo", getLogo());
-       parametros.put("oficina",getOficina());
-       // parametros.put("USUARIO","miguel" ); 
-	repor.addMapParam(parametros);
-	rpt=repor.ejecutaReporte(context,serveltcontext);		  
-if(!rpt && message==null){
-	//no tiene hojas	
-	message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje","No hay datos para generar reporte");
-	FacesContext.getCurrentInstance().addMessage(null, message);
-}
-}
+        parametros.put("oficina", getOficina());
+        // parametros.put("USUARIO","miguel" ); 
+        repor.addMapParam(parametros);
+        rpt = repor.ejecutaReporte(context, serveltcontext);
+        if (!rpt && message == null) {
+            //no tiene hojas	
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje", "No hay datos para generar reporte");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
 
+    public void mostrarReporteDocumentos() {
 
-public void mostrarReporteDocumentos(){
-
-context = FacesContext.getCurrentInstance (); 
-serveltcontext = (ServletContext) context.getExternalContext().getContext();
-ReporteController repor;
-HashMap<String, Object> parametros = new HashMap<String, Object>();
-parametros.clear();
-FacesContext context = FacesContext.getCurrentInstance (); 
-System.out.println("context"+context);
-ServletContext sc = (ServletContext) context.getExternalContext ().getContext();
-System.out.println("sc = "+sc.getRealPath ("/reportes/"));
-repor= ReporteController.getInstance("reporteDocumentos");
-categoriaServicio categoriaServicio= new categoriaServicio();
-repor.setConexion(categoriaServicio.getConexion() );
-repor.setTipoFormato(opcionFormato);   /// para tIPO FORMATO  08/05
-FacesMessage message=null;
-boolean rpt=false;	
+        context = FacesContext.getCurrentInstance();
+        serveltcontext = (ServletContext) context.getExternalContext().getContext();
+        ReporteController repor;
+        HashMap<String, Object> parametros = new HashMap<String, Object>();
+        parametros.clear();
+        FacesContext context = FacesContext.getCurrentInstance();
+        System.out.println("context" + context);
+        ServletContext sc = (ServletContext) context.getExternalContext().getContext();
+        System.out.println("sc = " + sc.getRealPath("/reportes/"));
+        repor = ReporteController.getInstance("reporteDocumentos");
+        categoriaServicio categoriaServicio = new categoriaServicio();
+        repor.setConexion(categoriaServicio.getConexion());
+        repor.setTipoFormato(opcionFormato);   /// para tIPO FORMATO  08/05
+        FacesMessage message = null;
+        boolean rpt = false;
         parametros.put("usuario", getUSUARIO());
         //parametros.put("oficina","oficina oli");
         parametros.put("logo", getLogo());
-        parametros.put("oficina",getOficina());
-       // parametros.put("USUARIO","miguel" ); 
-	repor.addMapParam(parametros);
-	rpt=repor.ejecutaReporte(context,serveltcontext);		  
-if(!rpt && message==null){
-	//no tiene hojas	
-	message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje","No hay datos para generar reporte");
-	FacesContext.getCurrentInstance().addMessage(null, message);
-}
-}
-/*
-public int getAnioActual() {
+        parametros.put("oficina", getOficina());
+        // parametros.put("USUARIO","miguel" ); 
+        repor.addMapParam(parametros);
+        rpt = repor.ejecutaReporte(context, serveltcontext);
+        if (!rpt && message == null) {
+            //no tiene hojas	
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje", "No hay datos para generar reporte");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+    /*
+     public int getAnioActual() {
 	
-	anioEstado = (AnioEstadoPresupuestal) Util.obtenerObjetoSession("anioGlobal");
-	anioActual= anioEstado.getAnio();
+     anioEstado = (AnioEstadoPresupuestal) Util.obtenerObjetoSession("anioGlobal");
+     anioActual= anioEstado.getAnio();
 
-	return anioActual;
-}*/
+     return anioActual;
+     }*/
 
-public void setAnioActual(int anioActual) {
-	this.anioActual = anioActual;
-}
+    public void setAnioActual(int anioActual) {
+        this.anioActual = anioActual;
+    }
 
     public FacesContext getContext() {
         return context;
@@ -243,59 +232,65 @@ public void setAnioActual(int anioActual) {
     public void setMesActual(int mesActual) {
         this.mesActual = mesActual;
     }
+
     public String getUSUARIO() {
-        String nombre="";
+        String nombre = "";
         context = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession) context.getExternalContext().getSession(true);
         Usuario usu = (Usuario) session.getAttribute("sesionUsuario");
-        nombre=usu.getUsuNombre();
+        nombre = usu.getUsuNombre();
         return nombre;
     }
-    
-    public String getOficina()
-    {
-        String nomOfi="";
+
+    public String getOficina() {
+        String nomOfi = "";
         Oficina ofi;
         context = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession) context.getExternalContext().getSession(true);
         Usuario usu = (Usuario) session.getAttribute("sesionUsuario");
         System.out.println("acaaaaaaaaaaaaaaa");
-        nomOfi=dd.getOficina(usu);
+        nomOfi = dd.getOficina(usu);
         System.out.println(nomOfi);
         return nomOfi;
     }
-    
-    
-    
-    public String getLogo(){	
-	String logo="";	
-	logo=serveltcontext.getRealPath("/resources/img/"+"escudo_reporte"+".jpg");	
-	return logo;
-}
-   /* public String obtenerUsuario(){
+
+    public String getLogo() {
+        String logo = "";
+        logo = serveltcontext.getRealPath("/resources/img/" + "escudo_reporte" + ".jpg");
+        return logo;
+    }
+    /* public String obtenerUsuario(){
 	
-	String nombre="";
+     String nombre="";
 	
-	try{
-		ExternalContext context = 
-		FacesContext.getCurrentInstance().getExternalContext();
-		HttpServletRequest request = 
-    	(HttpServletRequest) context.getRequest();
+     try{
+     ExternalContext context = 
+     FacesContext.getCurrentInstance().getExternalContext();
+     HttpServletRequest request = 
+     (HttpServletRequest) context.getRequest();
 		
-		Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");  
-		nombre = usuario.getUsuNombre();
+     Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");  
+     nombre = usuario.getUsuNombre();
 		
-	}catch(Exception e){
+     }catch(Exception e){
 		
-	}
+     }
 	
-	return nombre;
+     return nombre;
 	
 	
-}*/
+     }*/
 
     public void setUSUARIO(String USUARIO) {
         this.USUARIO = USUARIO;
+    }
+
+    public DocumentoDAO getDd() {
+        return dd;
+    }
+
+    public void setDd(DocumentoDAO dd) {
+        this.dd = dd;
     }
 
 }
