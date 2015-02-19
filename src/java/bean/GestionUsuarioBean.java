cbf7186450f770aa0edfe6105498351fac4e9115
MIGUELJAZZ <GIT999>
2015-02-19 08:50:10 -0500

    zoila

--- src/java/bean/GestionUsuarioBean.java
+++ src/java/bean/GestionUsuarioBean.java
@@ -108,8 +108,8 @@ public class GestionUsuarioBean implements Serializable {
             RequestContext.getCurrentInstance().showMessageInDialog(message);
             Limpiar();
         } else {
-            System.out.println(nuevapass+"-"+antiguapass);
-            if (nuevapass.equals(antiguapass)) {
+            System.out.println(nuevapass+"-"+antiguapass+"-"+aux.getClave());
+            if (antiguapass.equals(aux.getClave())) {
                 try {
                     aux.setClave(nuevapass);
                     gu.Cambiar(aux);
